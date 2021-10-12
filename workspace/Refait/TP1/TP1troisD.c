9c834f47565ed1a93f06352953ef57812fd71b00
Leslie-Douai <76811057+Leslie-Douai@users.noreply.github.com>
2021-10-12 09:26:58 +0200

    Fix
    Test sur i et pas j....

--- workspace/Refait/TP1/TP1troisD.c
+++ workspace/Refait/TP1/TP1troisD.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 void valeurDesDes(int N){
-    if(N<=0 || N >= 6*3){
+    if(N<=2 || N > 6*3){
         printf("%s \n", "Impossible ");
     }
     else{
         for(int i = 1; i<7;i++){
-            for(int j = 1; i<7;j++){
+            for(int j = 1; j<7;j++){
                 if(N-i-j <=6 && N-i-j >0){
                     printf("%i %i %i \n", i,j,N-i-j);
                 }
@@ -19,9 +19,9 @@ void valeurDesDes(int N){
 
 int main(){
     valeurDesDes(-3);
-    printf("%s \n","Autre test 4");
-    valeurDesDes(4);
     printf("%s \n","Autre test 19");
     valeurDesDes(19);
+    printf("%s \n","Autre test 4");
+    valeurDesDes(4);
     return 0;
 }
